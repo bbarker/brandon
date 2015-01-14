c48f162189d29f825d09dc750b8a6eb0d05db4d9
bbarker <beb82@cornell.edu>
2015-01-14 10:41:01 -0500

    adding a script to delete old files

--- gitadd.sh
+++ gitadd.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 git add .gitignore gitadd.sh .emacs* Documents/MATLAB/startup.m .bash_profile \
-        .gitconfig .Macaulay2 julia/julia-mode.el .aspell* .hgrc
+        .gitconfig .Macaulay2 julia/julia-mode.el .aspell* .hgrc \
+        .utils/delete_old_cul_backups.pl
         
