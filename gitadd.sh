04592ebe54e511fe7f7c552913ade00b9ec0de26
bbarker <beb82@cornell.edu>
2015-05-03 22:47:40 -0400

    adding a copyright header script

--- gitadd.sh
+++ gitadd.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 git add .gitignore gitadd.sh .emacs* Documents/MATLAB/startup.m .bash_profile \
         .gitconfig .Macaulay2 julia/julia-mode.el .aspell* .hgrc \
-        .utils/delete_old_cul_backups.pl
+        .utils/delete_old_cul_backups.pl bin/CopyrightHeader.py
         
